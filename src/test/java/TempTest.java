ebff824f8e47c6d30468e25c7f033edf30d4b0ca
outlaws-bai <outlaws@88.com>
2024-06-28 21:34:30 +0800

    clean code

--- src/test/java/TempTest.java
+++ src/test/java/TempTest.java
@@ -1,5 +1,5 @@
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.m2sec.common.Log;
 import org.m2sec.common.Render;
 import org.m2sec.common.models.Request;
 
@@ -12,17 +12,17 @@ import java.util.Map;
  * @date: 2024/6/21 20:23
  * @description:
  */
+@Slf4j
 public class TempTest {
 
-    Log log = new Log(TempTest.class);
 
     @Test
     public void test() throws IOException {
 
         Request request = Request.of("http://192.168.1.4:8000/getUserInfo");
-        System.out.println("192.168.1.4".equals(request.getHost()));
-        System.out.println(Render.renderExpression("request.getHeaders().put(\"X-Request-Id\",java.util.UUID" +
-            ".randomUUID().toString())", new HashMap<>(Map.of("request", request))));
-        System.out.println(request);
+        log.info("{}","192.168.1.4".equals(request.getHost()));
+        Render.renderExpression("request.getHeaders().put(\"X-Request-Id\",java.util.UUID" +
+            ".randomUUID().toString())", new HashMap<>(Map.of("request", request)));
+        log.info("{}",request);
     }
 }
