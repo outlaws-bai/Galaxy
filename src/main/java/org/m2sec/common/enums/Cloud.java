b508fc9637aa85444922954068bcc3bcb99a1683
outlaws-bai <outlaws@88.com>
2024-06-30 21:28:59 +0800

    update config.default.yaml && md

---
+++ src/main/java/org/m2sec/common/enums/Cloud.java
@@ -0,0 +1,11 @@
+package org.m2sec.common.enums;
+
+/**
+ * @author: outlaws-bai
+ * @date: 2024/6/30 20:52
+ * @description:
+ */
+
+public enum Cloud {
+    ALI, AWS, HUAWEI, AZURE, GCP, TENCENT
+}
