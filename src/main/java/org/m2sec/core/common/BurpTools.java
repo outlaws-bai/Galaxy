dd7ca1f4b7b1c47b3014c34d6f63d286e994d27f
outlaws-bai <outlaws@88.com>
2024-07-13 12:10:27 +0800

    update generateCollaboratorPayload

--- src/main/java/org/m2sec/core/common/BurpTools.java
+++ src/main/java/org/m2sec/core/common/BurpTools.java
@@ -1,6 +1,7 @@
 package org.m2sec.core.common;
 
 import burp.api.montoya.MontoyaApi;
+import org.m2sec.Galaxy;
 import org.m2sec.core.utils.ByteUtil;
 
 /**
@@ -12,11 +13,19 @@ public class BurpTools {
 
     public static final char BURP_INTRUDER_VAR_FLAG = '§';
 
+    private static String DNS_LOG_ROOT_DOMAIN;
+
     public static String getIntruderWrappedText(byte[] data) {
         return ByteUtil.getWrappedText(data, BURP_INTRUDER_VAR_FLAG);
     }
 
     public static String generateCollaboratorPayload(MontoyaApi api) {
-        return api.collaborator().defaultPayloadGenerator().generatePayload().toString();
+        if (Galaxy.isInBurp()) {
+            if (DNS_LOG_ROOT_DOMAIN == null) {
+                DNS_LOG_ROOT_DOMAIN = api.collaborator().defaultPayloadGenerator().generatePayload().toString();
+            }
+            return Helper.generateRandomString(8) + "." + DNS_LOG_ROOT_DOMAIN;
+        }
+        return Helper.generateRandomString(32);
     }
 }
