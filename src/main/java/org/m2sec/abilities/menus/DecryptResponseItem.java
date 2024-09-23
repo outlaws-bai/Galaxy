bacb346106bb7a1fae1ba2467ae8bb6916246d62
outlaws <outlaws@88.com>
2024-09-24 00:05:45 +0800

    解密响应时，利用其请求进行表达式检查，以提示用户表达式的问题

--- src/main/java/org/m2sec/abilities/menus/DecryptResponseItem.java
+++ src/main/java/org/m2sec/abilities/menus/DecryptResponseItem.java
@@ -1,15 +1,21 @@
 package org.m2sec.abilities.menus;
 
 import burp.api.montoya.MontoyaApi;
+import burp.api.montoya.http.message.requests.HttpRequest;
 import burp.api.montoya.http.message.responses.HttpResponse;
 import burp.api.montoya.ui.contextmenu.ContextMenuEvent;
 import burp.api.montoya.ui.contextmenu.MessageEditorHttpRequestResponse;
 import org.m2sec.abilities.HttpHookHandler;
 import org.m2sec.core.common.Config;
 import org.m2sec.core.common.Constants;
+import org.m2sec.core.common.Render;
 import org.m2sec.core.common.SwingTools;
 import org.m2sec.core.models.Headers;
+import org.m2sec.core.models.Request;
 import org.m2sec.core.models.Response;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author: outlaws-bai
@@ -44,8 +50,18 @@ public class DecryptResponseItem extends IItem {
         HttpResponse httpResponse = messageEditorHttpRequestResponse.requestResponse().response();
         Response response = Response.of(httpResponse);
         Headers headers = response.getHeaders();
+        HttpRequest httpRequest = messageEditorHttpRequestResponse.requestResponse().request();
+        Request request = Request.of(httpRequest);
         if (headers.hasIgnoreCase(Constants.HTTP_HEADER_HOOK_HEADER_KEY)) {
             SwingTools.showInfoDialog("The response has been decrypted.");
+            return;
+        }
+        String expression = config.getOption().getRequestCheckExpression();
+        if (expression == null || expression.isBlank() || !(Boolean) Render.renderExpression(expression,
+            new HashMap<>(Map.of("request", request)))) {
+            SwingTools.showInfoDialog("The result of using this response's request to execute the check expression is" +
+                " false. " +
+                "Please check.");
             return;
         }
         HttpResponse newResponse = HttpHookHandler.hooker.tryHookResponseToBurp(httpResponse, 0, true);
